68ffe487369f3bb0beedccef0c80057cb9453231
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-07-24 07:44:50 +0200

    made Alarm parcelable

--- app/src/main/java/com/ancientlore/memento/Alarm.kt
+++ app/src/main/java/com/ancientlore/memento/Alarm.kt
@@ -3,9 +3,32 @@ package com.ancientlore.memento
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.os.Parcel
+import android.os.Parcelable
 import java.util.*
 
 @Entity(tableName = "alarms")
 data class Alarm(@PrimaryKey(autoGenerate = true) var  id: Long = 0,
 				 @field:ColumnInfo var title: String = "",
-				 @field:ColumnInfo var date: Date)
+				 @field:ColumnInfo var date: Date): Parcelable {
+
+	constructor(parcel: Parcel) : this(
+			parcel.readLong(),
+			parcel.readString(),
+			parcel.readSerializable() as Date)
+
+	override fun writeToParcel(parcel: Parcel, flags: Int) {
+		parcel.writeLong(id)
+		parcel.writeString(title)
+		parcel.writeSerializable(date)
+	}
+
+	override fun describeContents() = 0
+
+	companion object CREATOR : Parcelable.Creator<Alarm> {
+
+		override fun createFromParcel(parcel: Parcel) = Alarm(parcel)
+
+		override fun newArray(size: Int) = arrayOfNulls<Alarm>(size)
+	}
+}
