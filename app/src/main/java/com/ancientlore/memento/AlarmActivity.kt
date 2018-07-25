2249054cecd15c0fe8958f69cf1e78a59bd024fa
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-07-25 09:05:16 +0200

    implemented new alarm creation

--- app/src/main/java/com/ancientlore/memento/AlarmActivity.kt
+++ app/src/main/java/com/ancientlore/memento/AlarmActivity.kt
@@ -1,11 +1,22 @@
 package com.ancientlore.memento
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
 import com.ancientlore.memento.databinding.ActivityAlarmBinding
 
 class AlarmActivity: BaseActivity<ActivityAlarmBinding, AlarmActivityViewModel>() {
 
 	companion object {
 		const val EXTRA_ALARM = "alarm"
+	}
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		viewModel.submitAlarmEvent()
+				.take(1)
+				.subscribe { submitAlarm(it) }
 	}
 
 	override fun getLayoutId() = R.layout.activity_alarm
@@ -15,4 +26,11 @@ class AlarmActivity: BaseActivity<ActivityAlarmBinding, AlarmActivityViewModel>(
 	override fun createViewModel() = AlarmActivityViewModel()
 
 	override fun getTitleId() = R.string.new_alarm
+
+	private fun submitAlarm(alarm: Alarm) {
+		val intent = Intent()
+		intent.putExtra(EXTRA_ALARM, alarm)
+		setResult(Activity.RESULT_OK, intent)
+		finish()
+	}
 }
--- app/src/main/java/com/ancientlore/memento/AlarmActivityViewModel.kt
+++ app/src/main/java/com/ancientlore/memento/AlarmActivityViewModel.kt
@@ -1,5 +1,34 @@
 package com.ancientlore.memento
 
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import android.databinding.ObservableInt
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import java.util.*
 
-class AlarmActivityViewModel: ViewModel() {}
+
+class AlarmActivityViewModel: ViewModel() {
+
+	val title = ObservableField<String>("")
+
+	val hours = ObservableInt(0)
+	val minutes = ObservableInt(0)
+	val seconds = ObservableInt(1)
+
+	private val submitAlarmEvent = PublishSubject.create<Alarm>()
+
+	fun onSubmitClicked() { submitAlarmEvent.onNext(createAlarm()) }
+
+	fun submitAlarmEvent() = submitAlarmEvent as Observable<Alarm>
+
+	private fun createAlarm() = Alarm(0, title.get()!!, getDate())
+
+	private fun getDate(): Date {
+		val presentTime = Calendar.getInstance()
+		val calendar = GregorianCalendar()
+		calendar.set(presentTime.get(Calendar.YEAR), presentTime.get(Calendar.MONTH), presentTime.get(Calendar.DATE),
+				hours.get(), minutes.get(), seconds.get())
+		return calendar.time
+	}
+}
