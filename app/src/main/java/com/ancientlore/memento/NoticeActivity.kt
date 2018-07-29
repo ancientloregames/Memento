1430a88b1a7270ca731449696ce1ce0ba2c3e767
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-07-29 11:35:30 +0200

    added basic dismissing ui

--- app/src/main/java/com/ancientlore/memento/NoticeActivity.kt
+++ app/src/main/java/com/ancientlore/memento/NoticeActivity.kt
@@ -1,8 +1,17 @@
 package com.ancientlore.memento
 
+import android.os.Bundle
 import com.ancientlore.memento.databinding.ActivityNoticeBinding
 
 class NoticeActivity: BaseActivity<ActivityNoticeBinding, NoticeActivityViewModel>() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		viewModel.dismissAlarmEvent()
+				.take(1)
+				.subscribe { finish() }
+	}
 
 	override fun getLayoutId() = R.layout.activity_notice
 
--- app/src/main/java/com/ancientlore/memento/NoticeActivityViewModel.kt
+++ app/src/main/java/com/ancientlore/memento/NoticeActivityViewModel.kt
@@ -1,5 +1,14 @@
 package com.ancientlore.memento
 
 import android.arch.lifecycle.ViewModel
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 
-class NoticeActivityViewModel: ViewModel()
+class NoticeActivityViewModel: ViewModel() {
+
+	private val dismissAlarmEvent = PublishSubject.create<Any>()
+
+	fun onDismissClicked() { dismissAlarmEvent.onNext(Any()) }
+
+	fun dismissAlarmEvent() = dismissAlarmEvent as Observable<Any>
+}
