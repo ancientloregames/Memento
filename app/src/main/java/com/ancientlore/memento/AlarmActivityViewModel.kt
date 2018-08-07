d12c9a3b48697bed3eed16d418137b263a214fb5
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-08-07 08:28:52 +0200

    fixed View Model fields naming

--- app/src/main/java/com/ancientlore/memento/AlarmActivityViewModel.kt
+++ app/src/main/java/com/ancientlore/memento/AlarmActivityViewModel.kt
@@ -11,14 +11,14 @@ class AlarmActivityViewModel: ViewModel {
 
 	val id: Long
 
-	val title = ObservableField<String>("")
+	val titleField = ObservableField<String>("")
 	val messageField = ObservableField<String>("")
 
 	/* FIXME two-way dataBinding doesn't work properly at this time. The binding of the minute attribute
 	*  changes the hour filed in the View class https://issuetracker.google.com/issues/111948800
 	*/
-	val hours = ObservableInt(0)
-	val minutes = ObservableInt(0)
+	val hoursField = ObservableInt(0)
+	val minutesField = ObservableInt(0)
 
 	val periodTitle = ObservableField<String>("")
 
@@ -40,7 +40,7 @@ class AlarmActivityViewModel: ViewModel {
 
 	constructor(alarm: Alarm, periodTitle: String) {
 		id = alarm.id
-		title.set(alarm.title)
+		titleField.set(alarm.title)
 		messageField.set(alarm.message)
 
 		applyDate(alarm.date)
@@ -59,21 +59,21 @@ class AlarmActivityViewModel: ViewModel {
 
 	fun onDeleteClicked() { deleteAlarmEvent.onNext(id) }
 
-	private fun createAlarm() = Alarm(id, title.get()!!, messageField.get()!!, getDate(), currentPeriod, true)
+	private fun createAlarm() = Alarm(id, titleField.get()!!, messageField.get()!!, getDate(), currentPeriod, true)
 
 	private fun applyDate(date: Date?) {
 		val calendar = Calendar.getInstance()
 		date?.let { calendar.time = it }
-		hours.set(calendar.get(Calendar.HOUR_OF_DAY))
-		minutes.set(calendar.get(Calendar.MINUTE))
+		hoursField.set(calendar.get(Calendar.HOUR_OF_DAY))
+		minutesField.set(calendar.get(Calendar.MINUTE))
 	}
 
 	private fun getDate(): Date {
 		val calendar = Calendar.getInstance().apply {
 			val dayDelay = Alarm.getDaysToIncrement(get(Calendar.DAY_OF_WEEK), currentPeriod)
 			add(Calendar.DAY_OF_MONTH, dayDelay)
-			set(Calendar.HOUR_OF_DAY, hours.get())
-			set(Calendar.MINUTE, minutes.get())
+			set(Calendar.HOUR_OF_DAY, hoursField.get())
+			set(Calendar.MINUTE, minutesField.get())
 		}
 		return calendar.time
 	}
