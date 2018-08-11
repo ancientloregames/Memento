06a5ca62e8d8ee0a587d41f93a7a83bdbf703b21
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-08-11 14:53:42 +0200

    added vibration

--- app/src/main/java/com/ancientlore/memento/NoticeActivity.kt
+++ app/src/main/java/com/ancientlore/memento/NoticeActivity.kt
@@ -1,13 +1,23 @@
 package com.ancientlore.memento
 
+import android.content.Context
 import android.media.Ringtone
 import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import com.ancientlore.memento.databinding.ActivityNoticeBinding
 
 class NoticeActivity: BaseActivity<ActivityNoticeBinding, NoticeActivityViewModel>() {
 
+	companion object {
+		const val alarmDuration = 10000L
+	}
+
 	private var ringtone: Ringtone? = null
+	private var vibrator: Vibrator? = null
 
 	override fun getLayoutId() = R.layout.activity_notice
 
@@ -22,7 +32,8 @@ class NoticeActivity: BaseActivity<ActivityNoticeBinding, NoticeActivityViewMode
 
 		intent.getParcelableExtra<Alarm>(AlarmReceiver.EXTRA_ALARM)?.let {
 			initViewModel(it)
-			ringtone = RingtoneManager.getRingtone(this, it.sound).apply { play() }
+			playSound(it.sound)
+			if (it.withVibration) vibrate()
 		}
 	}
 
@@ -35,8 +46,23 @@ class NoticeActivity: BaseActivity<ActivityNoticeBinding, NoticeActivityViewMode
 				.subscribe { finish() }
 	}
 
+	private fun playSound(uri: Uri) {
+		ringtone = RingtoneManager.getRingtone(this, uri).apply { play() }
+	}
+
+	private fun vibrate() {
+		((getSystemService(Context.VIBRATOR_SERVICE)) as Vibrator)
+				.takeIf { it.hasVibrator() }?.run {
+					vibrator = this
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && hasAmplitudeControl())
+						vibrate(VibrationEffect.createOneShot(alarmDuration, VibrationEffect.DEFAULT_AMPLITUDE))
+					else vibrate(alarmDuration)
+				}
+	}
+
 	override fun finish() {
 		ringtone?.stop()
+		vibrator?.cancel()
 		super.finish()
 	}
 }
